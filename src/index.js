ff6ae73d6176f933129801ad1e842bb502d3176c
Gaston Ezequiel Campos <camposgaston@gmail.com>
2020-10-14 20:45:04 -0300

    ussing Prisma Client to connect to DB

--- src/index.js
+++ src/index.js
@@ -1,36 +1,34 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { PrismaClient } = require('@prisma/client')
 
-// 1
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-}]
-
-// 1
-let idCount = links.length
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews Clone`,
-        feed: () => links,
+        feed: async(parent, args, context) => {
+            return context.prisma.link.findMany()
+        },
     },
     Mutation: {
-        // 2
-        post: (parent, args) => {
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
-                url: args.url,
-            }
-            links.push(link)
-            return link
-        }
+        post: (parent, args, context) => {
+            const newLink = context.prisma.link.create({
+                data: {
+                    url: args.url,
+                    description: args.description,
+                },
+            })
+            return newLink
+        },
     },
 }
 
 // 3
+const prisma = new PrismaClient()
+
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers,
+    context: {
+        prisma,
+    }
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
