a772a1afaa3052828cb5d1dd139e646feb6caa91
Gaston Ezequiel Campos <camposgaston@gmail.com>
2020-10-14 23:01:49 -0300

    imported resolvers

--- src/index.js
+++ src/index.js
@@ -1,24 +1,16 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { PrismaClient } = require('@prisma/client')
+    // Resolvers
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
 
 const resolvers = {
-    Query: {
-        info: () => `This is the API of a Hackernews Clone`,
-        feed: async(parent, args, context) => {
-            return context.prisma.link.findMany()
-        },
-    },
-    Mutation: {
-        post: (parent, args, context) => {
-            const newLink = context.prisma.link.create({
-                data: {
-                    url: args.url,
-                    description: args.description,
-                },
-            })
-            return newLink
-        },
-    },
+    Query,
+    Mutation,
+    User,
+    Link
 }
 
 // 3
