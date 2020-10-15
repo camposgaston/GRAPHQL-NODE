6fc7cc8fd00e34cac1896f236d692575ffabeee7
Gaston Ezequiel Campos <camposgaston@gmail.com>
2020-10-14 22:49:21 -0300

    Creating the context as a function which returns the context

--- src/index.js
+++ src/index.js
@@ -27,8 +27,16 @@ const prisma = new PrismaClient()
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers,
-    context: {
-        prisma,
-    }
+    // Creating the context as a function which returns the context. 
+    // The advantage of this approach is that you can attach the HTTP request that carries 
+    // the incoming GraphQL query (or mutation) to the context as well. 
+    // This will allow your resolvers to read the Authorization header and validate if 
+    // the user who submitted the request is eligible to perform the requested operation.
+    context: request => {
+        return {
+            ...request,
+            prisma,
+        }
+    },
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
